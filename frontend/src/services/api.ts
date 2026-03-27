644b49b9fe120cefb8c8df8bacdff2d884dc4b27
Alex1um <ye4h@pm.me>
2026-03-27 17:13:51 +0700

    fixed api.ts to work with any base url

--- frontend/src/services/api.ts
+++ frontend/src/services/api.ts
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const API_BASE = import.meta.env.VITE_API_URL || "http://localhost:7860";
+const API_BASE = import.meta.env.VITE_API_URL || "";
 
 const api = axios.create({
   baseURL: `${API_BASE}/api`,
