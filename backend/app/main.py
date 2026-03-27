67d5e964dd4f78a5ec0957f445a69ecfa60bd94e
Alex1um <ye4h@pm.me>
2026-03-27 17:31:25 +0700

    trying with any origin

--- backend/app/main.py
+++ backend/app/main.py
@@ -38,9 +38,7 @@ app = FastAPI(
 app.add_middleware(
     CORSMiddleware,
     allow_origins=[
-        settings.frontend_url,
-        "http://localhost:5173",
-        "http://localhost:7860",
+        "*"
     ],
     allow_credentials=True,
     allow_methods=["*"],
